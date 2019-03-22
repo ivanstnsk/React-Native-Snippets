4611c73f8e29d54bf8f73fe85b9740b2c7288ace
Airomad <airomad7@gmail.com>
2019-03-22 20:21:20 +0200

    Added 'Detect iPhone X' snippet

---
+++ DetectIphoneX.js
@@ -0,0 +1,21 @@
+import { Dimensions, Platform } from 'react-native';
+
+export function isIphoneX() {
+  let dimen = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    (dimen.height === 812 || dimen.width === 812)
+  );
+}
+
+export function isIphoneXorAbove() {
+  const dimen = Dimensions.get('window');
+  return (
+    Platform.OS === 'ios' &&
+    !Platform.isPad &&
+    !Platform.isTVOS &&
+    ((dimen.height === 812 || dimen.width === 812) || (dimen.height === 896 || dimen.width === 896))
+  );
+}
